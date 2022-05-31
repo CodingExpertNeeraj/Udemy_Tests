62e8e1abd1fc894129df1bccf8fdc3619df52537
Neeraj Tiwari <nt98924@gmail.com>
2022-05-31 12:20:51 +0530

    new test file added

---
+++ js/test2.js
@@ -0,0 +1,6 @@
+//  document.getElementById("container").style= "align:center";
+// document.getElementById('container').setAttribute('align','center);
+// document.getElementById('container').setAttribute('align', 'center');
+
+document.querySelector("img").style="width:150px;border-radius:50%;";
+document.getElementById("container").style="text-align:center";
